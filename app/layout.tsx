54ecd64717c69ca21b1651f55deaab64ece7055e
Kayle54187 <habichristi@gmail.com>
2023-09-11 01:19:17 +0200

    Adding Modal Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem={true}
             storageKey='discord-theme'
           >
+            <ModalProvider/>
             {children}
           </ThemeProvider>
         </body>
