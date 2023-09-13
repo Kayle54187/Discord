e297c61a858a0f16fc79386ad27682b53fdfc019
Kayle54187 <habichristi@gmail.com>
2023-09-14 01:20:31 +0200

    Adding server invite modal

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle
+} from '@/components/ui/dialog';
+
+import { useModal } from '@/hooks/use-modal-store';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Check, Copy, RefreshCcw } from 'lucide-react';
+import { useOrigin } from '@/hooks/use-origin';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+
+
+const InviteModal = () => {
+
+    const { isOpen, onOpen, onClose, type, data } = useModal();
+    const isModalOpen = isOpen && type === 'invite';
+    const origin = useOrigin();
+
+    const { server } = data;
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        },2000);
+    }
+
+    const onNew = async () => {
+        try{
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+            onOpen('invite', { server: response.data });
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    return ( 
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+                <DialogHeader className='pt-8 px-6'>
+                        <DialogTitle className='text-2xl text-center'>
+                            Invite Friends
+                        </DialogTitle>
+                </DialogHeader>
+                <div className='p-6'>
+                    <Label className='uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70'>
+                        Server Invite Link
+                    </Label>
+                    <div className='flex items-center mt-2 gap-x-2'>
+                        <Input
+                            className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
+                            value={inviteUrl}
+                            disabled={isLoading}
+                        />
+                        <Button size='icon' onClick={onCopy}>
+                            {
+                                copied ? <Check className='w-4 h-4'/> : <Copy className='w-4 h-4'/>
+                            }
+                        </Button>
+                    </div>
+                    <Button variant='link' size='sm' className='text-sm text-zinc-500 mt-4'  disabled={isLoading}>
+                        Generate a new link
+                        <RefreshCcw className='w-4 h-4 ml-[2px]'/>
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
+     );
+}
+ 
+export default InviteModal;
